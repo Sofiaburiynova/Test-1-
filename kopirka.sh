4203e0af0a07c9c9b2444d3e7aabaf7dede2f30c
Sofiaburiynova <167516259+Sofiaburiynova@users.noreply.github.com>
2024-04-23 21:45:12 +0300

    Update kopirka.sh

--- kopirka.sh
+++ kopirka.sh
@@ -10,5 +10,13 @@ directoriesinDirectory=$(find $input_dir -type d -maxdepth 1 -mindepth 1)
 files=$(find $input_dir -type f)
 for i in $files
 do 
-    cp $i $output_dir
+    filename=$(basename "$i")
+    if [ -e "$output_dir/$filename" ]
+    then
+        new_filename="${filename%.*}_$(date +%s%N).${filename##*.}"
+    else
+        new_filename="$filename"
+    fi
+    
+    cp $i $output_dir/$new_filename
 done
