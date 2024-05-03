5705e2c906a5d1c6e8269cf2ebbccc8e5bdc13f2
Sofiaburiynova <167516259+Sofiaburiynova@users.noreply.github.com>
2024-05-03 15:30:49 +0300

    обновления названия файла с совпадающим названием с помощью рекурсивной функции

--- kopirka.sh
+++ kopirka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-##  Сначала скрипт проверяет, что количество переданных аргументов равно 2
-if [[ $# -ne 2 ]]; then
+##проверка на то, что было переданно 2 аргументва
+if [ "$#" -ne 2 ]; then
     echo "Использование: $0 <входная_директория> <выходная_директория>"
     exit 1
 fi
@@ -9,40 +9,49 @@ fi
 input_dir=$1
 output_dir=$2
 
-##проверка прав доступа 
-    if [[ ! -x "$input_dir" ]]; then
-        echo "Директория $input_dir не имеет прав на выполнение."
-        exit 1
-    fi
-      
+##проверка на существование директории 
+if [ ! -d "$input_dir" ]; then
+    echo "Директория $input_dir не имеет прав на выполнение."
+    exit 1
+fi
 
 IFS=$'\n'
-##С помощью команды find скрипт ищет файлы и директории во входной директории
+##c помощью команды find скрипт ищет файлы и директории во входной директории.
 filesInDirectory=$(find $input_dir -type f -maxdepth 1)
 ##проверка на скрытность файлов
 filesInDirectory=$(find $input_dir -type f -maxdepth 1 -not -name '.*')   
-##Переменные filesInDirectory и directoriesinDirectory содержат список файлов и директорий
+##Переменные filesInDirectory и directoriesinDirectory содержат список файлов и директорий соответственно.
 directoriesinDirectory=$(find $input_dir -type d -maxdepth 1 -mindepth 1)
 files=$(find $input_dir -type f)
 
 
 
-##Для каждого файла во входной директории:Получается имя файла с помощью basename
-        ##Проверяется, существует ли файл с таким же именем в выходной директории
-        ##Если файл существует, создается новое имя файла, добавляя текущее время в миллисекундах к базовому имени файла
-        ##Иначе используется базовое имя файла
-        ##Файл копируется в выходную директорию с новым именем
+##скрипт рекурсивно ободит файлы, это нужно для того, что если встретилась директория, то скрипт будет рекурсивно запущен. 
+##если файлы имеют одинаковое название, то скрипт добавит число прерд названием файла 
+cp_rename() {
+    local src_dir="$1"
+    local dest_dir="$2"
+    local files=("$src_dir"/*)
 
-for i in $files
-do 
-    filename=$(basename "$i")
-    if [[ -e "$output_dir/$filename" ]]
-    then
-        new_filename="${filename%.*}_$(date +%s%N).${filename##*.}"
-    else
-        new_filename="$filename"
-    fi
-    
-    cp $i $output_dir/$new_filename
-done
-##Скрипт завершает выполнение
+    for file in "${files[@]}"; do
+        if [ -d "$file" ]; then
+            cp_rename "$file" "$dest_dir"
+        elif [ -f "$file" ]; then
+            filename=$(basename -- "$file")
+            dest_file="$dest_dir/${filename}"
+
+            if [ -f "$dest_file" ]; then
+                i=1
+                while [ -f "$dest_dir/($i) ${filename}" ]; do
+                    ((i++))
+                done
+                cp "$file" "$dest_dir/($i) ${filename} "
+            else
+                cp "$file" "$dest_file"
+            fi
+        fi
+    done
+}
+
+cp_rename "$input_dir" "$output_dir"
+
